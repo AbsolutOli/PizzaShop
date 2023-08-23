99a91f239cc0edbc7335e10553817947a7fa8ee6
AbsolutOli <vocameoli@gmail.com>
2023-08-23 20:37:22 +0300

    Added different types of pizza

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from "./assets/pizzas.json";
+
 function App() {
   return (
     <div className="wrapper">
@@ -15,15 +17,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+            {pizzas.map((item) => (
+              <PizzaBlock {...item} />
+            ))}
           </div>
         </div>
       </div>
