55ef0a57ffc7fd75c4cc269c3142096901a3f936
AbsolutOli <vocameoli@gmail.com>
2023-08-24 14:50:32 +0300

    Added geting pizzas array from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
+
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
+  const [pizzasArr, setPizzasArr] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://64e73e4cb0fd9648b78f9b4b.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => setPizzasArr(arr));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,7 +25,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((item) => (
+            {pizzasArr.map((item) => (
               <PizzaBlock key={item.id} {...item} />
             ))}
           </div>
